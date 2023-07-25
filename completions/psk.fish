5f7cdce7015ceb4ef21357e84c80d7bedae7fda5
Alex Rutar <alex@rutar.org>
2023-07-25 10:10:36 +0100

    fix completions

--- completions/psk.fish
+++ completions/psk.fish
@@ -1,4 +1,4 @@
-set -l psk_subcommands login copy-key show-key list-keys
+set -l psk_subcommands login copy show list
 complete --command psk --exclusive --long help --description "Print help"
 complete --command psk --exclusive --long version --description "Print version"
 
@@ -23,11 +23,15 @@ complete --command psk --exclusive --argument login \
     -n __fish_use_subcommand \
     --description 'Copy login and pass to the clipboard'
 
-complete --command psk --exclusive --argument copy-key \
+complete --command psk --exclusive --argument copy \
     -n __fish_use_subcommand \
     --description 'Copy value of key to the clipboard'
 
-complete --command psk --exclusive --argument list-keys \
+complete --command psk --exclusive --argument show \
+    -n __fish_use_subcommand \
+    --description 'Print value of key'
+
+complete --command psk --exclusive --argument list \
     -n __fish_use_subcommand \
     --description 'List valid keys'
 
